057ece32e9cef61c63c006033f0d7491be0910a4
DoQuocBinh <binhdq@fpt.edu.vn>
2020-09-15 08:35:14 +0700

    day 3 demo code

---
+++ firstDay/demoDay3/main.js
@@ -0,0 +1,4 @@
+const myMath= require('./myModule.js');
+
+console.log(myMath.double(3));
+console.log(myMath.rectangleArea(5,9));
---
+++ firstDay/demoDay3/myModule.js
@@ -0,0 +1,10 @@
+var MathLib ={
+    double : function(x){
+        return x*2;
+    },
+
+    rectangleArea : function(width, height){
+        return width*height;
+    }
+};
+module.exports = MathLib;

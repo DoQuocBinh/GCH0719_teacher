a2e42f40a68bf31aa65d30f2c9b730fd936518e3
DoQuocBinh <binhdq@fpt.edu.vn>
2020-09-15 10:34:04 +0700

    aa

--- demoDay3/server.js
+++ demoDay3/server.js
@@ -7,7 +7,7 @@ var server = http.createServer(function (req, res) {   //create web server
         res.writeHead(200, { 'Content-Type': 'text/html' }); 
         
         // set response content    
-        res.write('<html><body><p style="color:red">This is home Page.</p></body></html>');
+        res.write('<html><body><p style="color:blue">This is home Page.</p></body></html>');
         res.end();
     
     }

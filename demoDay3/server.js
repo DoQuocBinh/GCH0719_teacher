f929301677faa0778239678e0256241146f87728
DoQuocBinh <binhdq@fpt.edu.vn>
2020-09-15 10:40:04 +0700

    aaa

--- demoDay3/server.js
+++ demoDay3/server.js
@@ -24,6 +24,12 @@ var server = http.createServer(function (req, res) {   //create web server
         res.write('<html><body><p>This is admin Page.</p></body></html>');
         res.end();
     
+    }else if(req.url=='/login'){
+        let textAndButt =
+             "<input type='text' size='20'/><input type='button' value='Login'/>";
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(`<html><body><p>This is login Page.</p>${textAndButt}</body></html>`);
+        res.end();
     }
     else
         res.end('Invalid Request!');

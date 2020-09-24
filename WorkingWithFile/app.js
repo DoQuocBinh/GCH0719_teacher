d9c8c44a57fc4c632d56e19b7248b4e79588041e
DoQuocBinh <binhdq@fpt.edu.vn>
2020-09-24 11:28:16 +0700

    aaaa

--- WorkingWithFile/app.js
+++ WorkingWithFile/app.js
@@ -3,18 +3,43 @@ const PORT = process.env.PORT || 5000;
 const path = require('path')
 
 var app= express()
+var fs = require('fs');
 
 //localhost:5000
 app.get('/',function(req,res){
    let p = path.join(__dirname) + '/index.html';
    res.sendFile(p);
 })
-var fs = require('fs')
+
+
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/register',function(req,res){
     let p = path.join(__dirname) + '/register.html';
     res.sendFile(p);
+})
+app.get('/login',function(req,res){
+    let p = path.join(__dirname) + '/login.html';
+    res.sendFile(p);
+})
+var fspromises = require('fs').promises;
+app.post('/doLogin',async function(req,res){
+    //get what user entered into form
+    let nameInput = req.body.txtName;
+    let phoneInput = req.body.txtPhone;
+    let fileContent ='';
+    content= await fspromises.readFile('users.txt','utf-8');
+    let usersText = content.trim().split(os.EOL);
+    for(i=0;i<usersText.length;i++){
+       let eachUser = usersText[i];
+        let name = eachUser.split(';')[0];
+        let phone =  eachUser.split(';')[1];
+        if(name== nameInput && phone == phoneInput){
+            res.end('Correct user!');
+            return;
+        }
+    }
+    res.end('Invalid user');
 })
 
 app.get('/allUsers',async function(req,res){

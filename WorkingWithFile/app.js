2c35439efd92a7264f2b4e8118a0e7ba5ac2154d
DoQuocBinh <binhdq@fpt.edu.vn>
2020-09-24 09:22:07 +0700

    aa

--- WorkingWithFile/app.js
+++ WorkingWithFile/app.js
@@ -9,6 +9,27 @@ app.get('/',function(req,res){
    let p = path.join(__dirname) + '/index.html';
    res.sendFile(p);
 })
+var fs = require('fs')
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.get('/register',function(req,res){
+    let p = path.join(__dirname) + '/register.html';
+    res.sendFile(p);
+})
+app.post('/doRegister',function(req,res){
+    //get what user entered into form
+    let name = req.body.txtName;
+    let phone = req.body.txtPhone;
+    //write to file
+    let content= name + ';' + phone;
+    fs.appendFile('users.txt',content,function(err){
+        if(err)
+            res.end('Something is wrong!');
+        else
+            res.end('Done! Check the file for update')
+    })
+
+})
 
 app.listen(PORT,function(){
     console.log("Server is running!")

64d8f78e950e7d03a50af8f57f177181452da2f4
Binh Do Quoc <binhdq@fpt.edu.vn>
2020-10-06 09:09:52 +0700

    new and delete

--- ExpressJSDemo/app.js
+++ ExpressJSDemo/app.js
@@ -24,11 +24,58 @@ app.get('/all',async function(req,res){
         var products = JSON.parse(data).products;
         res.render('allProduct',{model:products});
     } catch (error) {
-        console.log('something is wrong; readFile');
+        console.log('something is wrong; readFile' + error);
     }
 })
 app.get('/add', function(req,res){
     res.render('addProduct');
+})
+app.post('/doAddProduct',async function(req,res){
+    //load old database
+    try {
+        //read from file and load to memory
+        const data = await fs.readFile('database.json');
+        let products = JSON.parse(data).products;
+        //from user input
+        let id = req.body.txtId;
+        let name = req.body.txtName;
+        let price = req.body.Price;
+        let newProduct =   {"id": Number(id), "name": name, "price": price}
+        //update memory
+        products.push(newProduct);
+        var document = {'products': products}
+        //write memory to file
+        await fs.writeFile('database.json',JSON.stringify(document));
+        res.redirect('/all');
+
+    } catch (error) {
+        console.log('something is wrong; write' + error);
+    }
+})
+
+app.get('/delete',async function (req,res){
+    let id = req.query.id;
+    let deleteIndex =-1;
+    //load file to memory(array)
+    try {
+        const data = await fs.readFile('database.json');
+        var products = JSON.parse(data).products;
+        for(i=0;i<products.length;i++){
+            if(products[i].id == id){
+               deleteIndex = i;
+                break;
+            }
+        }
+        products.splice(deleteIndex,1);
+        //save to file
+        var document = {'products': products}
+        //write memory to file
+        await fs.writeFile('database.json',JSON.stringify(document));
+        res.redirect('/all');
+        
+    } catch (error) {
+        console.log('something is wrong; delete file' + error);
+    }
 })
 
 var PORT = process.env.PORT || 5000;
